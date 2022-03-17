8984d6d9a17863ca245825cd31f2569b2b21da7f
SantiagoIvan <48731203+SantiagoIvan@users.noreply.github.com>
2022-03-17 19:57:31 -0300

    Create depthfirst_search.py

---
+++ depthfirst_search.py
@@ -0,0 +1,32 @@
+my_graph = {
+    "a": ["b", "d"],
+    "b": ["c"],
+    "c": [],
+    "d": ["b", "f"],
+    "e": ["d"],
+    "f": []
+}
+
+def deepthfirst_search_recursive(graph, node):
+    if node in graph.keys():
+        print(node.value)
+        for adj in graph[node]:
+            deepthfirst_search_recursive(graph, adj)
+            deepthfirst_search_recursive(graph, adj)
+    return
+
+def deepthfirst_search_iterative(graph, node):
+    result = []
+    stack = []
+    
+    stack.append(node)
+    while node and len(stack) > 0:
+        node = stack.pop()
+        result.append(node)
+        
+        if node in graph.keys():
+            for adj in graph[node]:
+                stack.append(adj)
+        
+    
+    return result
